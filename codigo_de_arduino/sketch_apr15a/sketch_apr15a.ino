5e2cc60365f0361467e2fb64c5fec93694d9abe7
ortez007 <albertpunisher@gmail.com>
2018-04-15 17:39:33 -0600

    codigo realizado de arduino

--- codigo_de_arduino/sketch_apr15a/sketch_apr15a.ino
+++ codigo_de_arduino/sketch_apr15a/sketch_apr15a.ino
@@ -10,5 +10,16 @@ Serial.begin(9600);
 void loop() {
 if (digitalRead(boton) == 0){
   Serial.println("HOLA A TODOS");
+  delay(500);
   }
+  if (Serial.available()){
+    char LETRA = Serial.read();
+    if (LETRA == "HOLA A TODOS"){
+      digitalWrite(led, HIGH);
+      }
+      else if (LETRA == 'L'){
+        digitalWrite(led, LOW);
+        }
+      }
+    }
 }
